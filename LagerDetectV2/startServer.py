04e051c40fce4b0dc638c7779fd528e4290925e2
Maksiom30 <maximlauk30@gmail.com>
2024-06-24 08:27:18 +0200

    add tabel

--- LagerDetectV2/startServer.py
+++ LagerDetectV2/startServer.py
@@ -4,18 +4,47 @@ import os
 from getImages import getImages
 from detect import detect_bottles
 
-class RoutedHandler(http.server.SimpleHTTPRequestHandler):
-        
+class Kasten():
+    id = 0
+    def __init__(self, name: str, max: int) -> None:
+        self.id = Kasten.id
+        self.name = name
+        self.max = max
+        self.nBottles = 0
+        Kasten.id += 1
 
+def init_data() -> list:  
+    kaesten = [
+        ["Früh Kölsch", 24],
+        ["Irish Ale", 24],
+        ["Spezi", 20],
+        ["Rhodius Classic", 24],
+        ["Bergmann Spezial", 20],
+        ["Pinkus Special", 20],
+        ["Fritz Cola", 24],
+        ["Club Mate", 20],
+        ["Bergmann Export", 20],
+        ["Fritz Cola", 24],
+        ["Club Mate Icetea", 20],
+        ["Bionade", 24]
+    ]
+    kasten_list = []
+    for kasten in kaesten:
+        kasten_list.append(Kasten(kasten[0], kasten[1]))
+    return kasten_list
+
+
+class RoutedHandler(http.server.SimpleHTTPRequestHandler):
+    kasten_list = init_data()
     def do_GET(self):
         getImages()
-        count = 0
-        for i in range(12):
-            count += detect_bottles(i+1)
-        
+        self.sumCount = 0
+        for kasten in self.kasten_list:
+            kasten.nBottles = detect_bottles(kasten.id + 1)
+            self.sumCount += kasten.nBottles
 
         if self.path == '/':
-            self.handle_root(count)
+            self.handle_root()
         elif self.path == '/img.jpg':
             self.handle_img()
         elif self.path == "/result.png":
@@ -25,15 +54,16 @@ class RoutedHandler(http.server.SimpleHTTPRequestHandler):
         else:
             self.handle_404()
 
-    def handle_root(self, bCount):
+    def handle_root(self):
         with open(os.path.join("/home/pi/LagerDetectV2", "templates/index.html"), "r") as file:
             html = file.read()
             file.close()
-        html = html.replace("{count1}", str(bCount))
-
-
-        
-
+        for kasten in self.kasten_list:
+            html = html.replace("{namex}". replace("x", str(kasten.id + 1)), kasten.name)
+            html = html.replace("{countx}".replace("x", str(kasten.id + 1)), str(kasten.nBottles))
+            html = html.replace("{empt_countx}".replace("x", str(kasten.id + 1)), str(kasten.max - kasten.nBottles))
+            html = html.replace("{prc_countx}".replace("x", str(kasten.id + 1)), str(round(kasten.nBottles / kasten.max * 100)) + "%")
+        html = html.replace("{count_all}", str(self.sumCount))
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
